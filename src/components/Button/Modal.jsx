80e67beb5ec02d40b160f1a5ea1729e4bcdd758b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-29 01:27:15 +0600

    modal image and description added

--- src/components/Button/Modal.jsx
+++ src/components/Button/Modal.jsx
@@ -2,21 +2,33 @@ import React from 'react';
 
 const Modal = (props) => {
     console.log(props.modalDataIs)
-    const {id} = props.modalDataIs ;
+    const { description, image_link , input_output_examples , features , integrations , pricing } = props.modalDataIs;
     return (
-    <>
-    <input type="checkbox" id="my-modal" className="modal-toggle" />
-     <div className="modal">
-     <div className="modal-box">
-     <h3 className="font-bold text-lg">{id}</h3>
-     <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-     <div className="modal-action">
-      <label htmlFor="my-modal" className="btn">Close</label>
-      </div>
-     </div>
-    </div>
-   </>
+        <>
+
+            <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
+                <div className='flex justify-between p-4'>
+                        <div className='w-[45%]'>
+                            <h2 className='text-xl font-bold'>{description}</h2>
+                            <div>
+
+                            </div>
+                        </div>
+                        <div className='w-[45%]'>
+                          <img src={image_link ? image_link[0] : 'No image found'} alt="" />
+                        </div>
+                    </div>
+                    <div className="modal-action">
+                        <label htmlFor="my-modal-5" className="btn">Close</label>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
 export default Modal;
+
+
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -37,7 +37,7 @@ const Cards = (props) => {
                             <p>{published_in}</p>
                         </div>
                     </div>
-                <label onClick={() => handlerId(id)} htmlFor="my-modal" className='cursor-pointer'>
+                <label onClick={() => handlerId(id)} htmlFor="my-modal-5" className='cursor-pointer'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                     </svg>
