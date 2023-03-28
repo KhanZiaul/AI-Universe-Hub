fd293dd5d7ae64ba212701a5cde95c525eba8e51
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-28 12:35:17 +0600

    packages added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <div >
+      <h2 className="text-3xl">welcome</h2>
+      <button className="btn btn-warning">Warning</button>
+    </div>
+  )
+}
+
+export default App
